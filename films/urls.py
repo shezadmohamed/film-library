0290562b94a28083d2467251263d1aa4774486e6
Shezad <shezadmohamed@gmail.com>
2024-11-22 00:19:06 +0000

    Add templates, modify urls and views

--- films/urls.py
+++ films/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "films"
 urlpatterns = [
-    path("", views.index, name="films_index"),
-    path("<int:film_id>/", views.film_detail, name="film_detail"),
-    path("<int:film_id>/reviews/", views.film_reviews, name="film_reviews")
+    path("", views.index, name="index"),
+    path("<int:film_id>/", views.detail, name="detail"),
+    path("<int:film_id>/reviews/", views.reviews, name="reviews")
 ]
